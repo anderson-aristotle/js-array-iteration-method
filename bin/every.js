98b5663883700a775d6026ceb88c1a1622004050
MicFin <mrfinneran@gmail.com>
2018-07-18 10:10:33 -0400

    Grunt nag
    Fixed grunt nag issues.  Used ``// eslint-disable-line no-unused-vars`
    for variables used as starter code
    
    Close #66

--- bin/every.js
+++ bin/every.js
@@ -3,13 +3,14 @@
 
 // 1.  Check if every one of the food items is more than 20 days old
 // using a predicate function called isRotten
-const fridge =  [
-     { food: 'Carrots', daysInFridge: 32 },
-     { food: 'Onions', daysInFridge: 23 },
-     { food: 'Bell Peppers', daysInFridge: 16 },
-     { food: 'Soda', daysInFridge: 7 },
-     { food: 'Pizza', daysInFridge: 4 },
-     { food: 'Cake', daysInFridge: 3 }
- ]
+const fridge = // eslint-disable-line no-unused-vars
+[
+  { food: 'Carrots', daysInFridge: 32 },
+  { food: 'Onions', daysInFridge: 23 },
+  { food: 'Bell Peppers', daysInFridge: 16 },
+  { food: 'Soda', daysInFridge: 7 },
+  { food: 'Pizza', daysInFridge: 4 },
+  { food: 'Cake', daysInFridge: 3 }
+]
 
 // LAB CODE GOES HERE
--- bin/some.js
+++ bin/some.js
@@ -3,13 +3,14 @@
 
 // 1.  Check if some of the foods more than 20 days old
 // using a predicate function called isRotten
-const fridge = [
-     { food: 'Carrots', daysInFridge: 32 },
-     { food: 'Onions', daysInFridge: 23 },
-     { food: 'Bell Peppers', daysInFridge: 16 },
-     { food: 'Soda', daysInFridge: 7 },
-     { food: 'Pizza', daysInFridge: 4 },
-     { food: 'Cake', daysInFridge: 3 }
+const fridge = // eslint-disable-line no-unused-vars
+[
+  { food: 'Carrots', daysInFridge: 32 },
+  { food: 'Onions', daysInFridge: 23 },
+  { food: 'Bell Peppers', daysInFridge: 16 },
+  { food: 'Soda', daysInFridge: 7 },
+  { food: 'Pizza', daysInFridge: 4 },
+  { food: 'Cake', daysInFridge: 3 }
 ]
 
 // CODE ALONG CODE GOES HERE
